63544c83e51deb77d66b99373d8634200455f1a7
AndileMnukwa <mnukwa1996@gmail.com>
2024-04-24 14:56:57 +0200

    mesages

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+// import { sendMessage, isTyping } from 'react-chat-engine';
+
 const MessageForm = () => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = () => {
+ 
+    }
+
+    const handleChange = (event) => {
+        // setValue(event.target.value);
+
+        // isTyping(props, chatId);
+    }
+
+
     return (
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input 
+                className="message-input"
+                placeholder="Send a message"
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     )
 }
 
