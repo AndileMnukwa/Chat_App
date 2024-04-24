20e157911e91b4923282201f4d7240b4e7c1f10d
AndileMnukwa <mnukwa1996@gmail.com>
2024-04-24 10:33:56 +0200

    Chat Engine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="515dafbe-0670-4819-bcfd-2053e577bc2c"
+            userName="reeman"
+            userSecret="12345678"
+            />
+    );
+}
+
+export default App;
