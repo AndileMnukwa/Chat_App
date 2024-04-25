d26948f71927d97ed1b585e47ed5f926e92d3fa3
mbasa mpohlolo <gqwedembasa@gmail.com>
2024-04-25 05:25:18 -0700

    css

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -28,7 +28,7 @@ try {
     return (
         <div className="wrapper">
             <div className="form">
-                <h1 className="title">Chat Application</h1>
+                <h1 className="title">Binary Brains 👋</h1>
                 <form onSubmit={handleSubmit}>
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
                     <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
