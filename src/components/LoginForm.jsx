6f45ceeb975ea256e169017bcb265b28393e939f
AndileMnukwa <mnukwa1996@gmail.com>
2024-04-26 11:22:02 +0200

    password

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -27,20 +27,35 @@ try {
 
     return (
         <div className="wrapper">
-            <div className="form">
-                <h1 className="title">Chat Application</h1>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
-                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
-                    <div align="center">
-                       <button type="submit" className="button">
-                            <span>Start Chatting</span>
-                        </button> 
-                    </div>
-                    <h2 className="error">{error}</h2>
-                </form>
-            </div>
+        <div className="form">
+            <h1 className="title">Chat Application</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
+                    className="input" 
+                    placeholder="Username" 
+                    required
+                />
+                <input 
+                    type="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    className="input" 
+                    placeholder="Password" 
+                    required
+                />
+                <div align="center">
+                    <button type="submit" className="button">
+                        <span>Start Chatting</span>
+                    </button> 
+                </div>
+                <h2 className="error">{error}</h2>
+            </form>
         </div>
+    </div>
+    
     )
 }
  export default LoginForm;
